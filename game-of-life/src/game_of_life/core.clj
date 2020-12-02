5b8db71a7ef7ef604646d9460f10976b32592bd8
Jordan Chapuy <chapuyj@gmail.com>
2020-12-02 19:02:25 +0100

    Add next generation cell. (Forgotten Commit :p)

--- game-of-life/src/game_of_life/core.clj
+++ game-of-life/src/game_of_life/core.clj
@@ -1,7 +1,29 @@
 (ns game-of-life.core
   (:gen-class))
 
+; rules
+
+(defn next-generation-for-alive-cell [numberOfAliveNeighbours]
+  (case numberOfAliveNeighbours
+    2 {:alive true}
+    3 {:alive true}
+    {:alive false}))
+
+(defn next-generation-for-dead-cell [numberOfAliveNeighbours]
+  (case numberOfAliveNeighbours
+    3 {:alive true}
+    {:alive false}))
+
+(defn next-generation-cell [cell numberOfAliveNeighbours]
+  (if (cell :alive)
+    (next-generation-for-alive-cell numberOfAliveNeighbours)
+    (next-generation-for-dead-cell numberOfAliveNeighbours)))
+
+; maybe i can use a map (enum -> lambda) instead of if
+
+; main
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [& _]
   (println "Hello, World!"))
--- game-of-life/test/game_of_life/core_test.clj
+++ game-of-life/test/game_of_life/core_test.clj
@@ -2,6 +2,88 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest rules
+
+  ; I need a way to fuzz, to have one test per rule.
+
+  ; First rule
+
+  (testing "Any live cell with fewer than two live neighbours dies, as if by underpopulation. (0)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 0))))
+
+  (testing "Any live cell with fewer than two live neighbours dies, as if by underpopulation. (1)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 1))))
+           
+  ; Second rule
+
+  (testing "Any live cell with two or three live neighbours lives on to the next generation. (2)"
+    (is (= {:alive true} 
+           (next-generation-cell {:alive true} 2))))
+
+  (testing "Any live cell with two or three live neighbours lives on to the next generation. (3)"
+    (is (= {:alive true} 
+           (next-generation-cell {:alive true} 3))))
+
+  ; Third rule
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation. (4)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 4))))
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation. (5)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 5))))
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation. (6)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 6))))
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation. (7)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 7))))
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation. (8)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive true} 8))))
+           
+  ; Fourth rule
+
+  (testing "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."
+    (is (= {:alive true} 
+           (next-generation-cell {:alive false} 3))))
+
+  ; The missing rule
+           
+  (testing "Any dead cell without exactly three live neighbours stays dead. (0)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 0))))   
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (1)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 1))))
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (2)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 2))))  
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (4)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 4))))
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (5)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 5))))   
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (6)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 6))))
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (7)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 7))))   
+
+  (testing "Any dead cell without exactly three live neighbours stays dead. (8)"
+    (is (= {:alive false} 
+           (next-generation-cell {:alive false} 8)))))
