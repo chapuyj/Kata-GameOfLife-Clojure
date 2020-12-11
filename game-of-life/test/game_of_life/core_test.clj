e809b6dfad013c6dd443aec84cbb2d222ab648c1
Jordan Chapuy <chapuyj@gmail.com>
2020-12-11 12:01:34 +0100

    Create grid from string

--- game-of-life/test/game_of_life/core_test.clj
+++ game-of-life/test/game_of_life/core_test.clj
@@ -27,27 +27,33 @@
 
 
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Move to new files (test and code)
+; a grid factory
+
 (defn isAlive [cell-string] (= "1" cell-string))
 
 (defn make-cell [line-number [column-number cell]] {:column column-number :line line-number})
 
 (defn make-cell-from-string-line [line-number columns] 
-  (let [alive-columns (filterv isAlive columns)
+  (let [alive-columns (filter isAlive columns)
         indexed-columns (map-indexed vector alive-columns)
         cells (map #(make-cell line-number %) indexed-columns)]
-    (set cells)))
+    cells))
 
 (deftest make-cell-from-string-line-test
 
   (testing "Create an empty line."
     (is (= (make-cell-from-string-line 7 ["0" "0" "0" "0"])
-           #{})))
+           `())))
   
   (testing "Create a full line."
     (is (= (make-cell-from-string-line 7 ["1" "1" "1"])
-           #{{:column 0 :line 7}
+           `({:column 0 :line 7}
              {:column 1 :line 7}
-             {:column 2 :line 7}}))))
+             {:column 2 :line 7})))))
 
 
 
@@ -68,7 +74,9 @@
 
 
 (defn from-string [string]
-  #{})
+  (let [indexed-lines (make-indexed-lines string)
+        cells (mapv #(make-cell-from-string-line (first %) (second %)) indexed-lines)]
+    (set (flatten cells))))
 
 (def empty-grid #{})
 
