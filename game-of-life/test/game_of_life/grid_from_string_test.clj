09c6e403058e681fcc0c6e30dac91763d6c6cc7d
Jordan Chapuy <chapuyj@gmail.com>
2020-12-12 18:14:03 +0100

    Small refactorings.

--- game-of-life/test/game_of_life/grid_from_string_test.clj
+++ game-of-life/test/game_of_life/grid_from_string_test.clj
@@ -4,8 +4,6 @@
 
 ; Behavior tests
 
-(def empty-grid-3 {:size 3 :alive-positions #{}})
-
 (def full-grid-3 {:size 3 :alive-positions #{{:column 0 :line 0}
                                              {:column 1 :line 0}
                                              {:column 2 :line 0}
@@ -22,7 +20,7 @@
     (is (= (from-string "0 0 0
                          0 0 0
                          0 0 0")
-           empty-grid-3)))
+           {:size 3 :alive-positions #{}})))
   
   (testing "Create an grid with vertical line from literate representation."
     (is (= (from-string "0 1 0
@@ -36,7 +34,9 @@
     (is (= (from-string "1 1 1
                          1 1 1
                          1 1 1")
-           full-grid-3))))
+           {:size 3 :alive-positions #{{:column 0 :line 0} {:column 1 :line 0} {:column 2 :line 0}
+                                       {:column 0 :line 1} {:column 1 :line 1} {:column 2 :line 1}
+                                       {:column 0 :line 2} {:column 1 :line 2} {:column 2 :line 2}}}))))
 
 ; Technical help tests
 
@@ -52,9 +52,7 @@
 
   (testing "Create a full line."
     (is (= (make-cell-from-string-line 7 ["1" "1" "1"])
-           `({:column 0 :line 7}
-             {:column 1 :line 7}
-             {:column 2 :line 7})))))
+           `({:column 0 :line 7} {:column 1 :line 7} {:column 2 :line 7})))))
 
 (deftest make-indexed-lines-test
 
