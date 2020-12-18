bca7ae9e70bf6ca92776dbaa30821916d7803029
Jordan Chapuy <chapuyj@gmail.com>
2020-12-18 16:45:03 +0100

    Cleaning project.clj

--- game-of-life/project.clj
+++ game-of-life/project.clj
@@ -1,8 +1,6 @@
-(defproject game-of-life "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject game-of-life "0.1.0"
+  :description "Game of Life"
+  :url "https://github.com/chapuyj/Kata-GameOfLife-Clojure"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/core.match "1.0.0"]]
   :main ^:skip-aot game-of-life.core
