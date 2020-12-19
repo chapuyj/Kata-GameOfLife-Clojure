ff183201ba79710775040c25315863abe6080915
Jordan Chapuy <chapuyj@gmail.com>
2020-12-19 16:08:52 +0100

    Tick cell.

--- grid/src/game_of_life/core.clj
+++ grid/src/game_of_life/core.clj
@@ -1,5 +1,17 @@
 (ns game-of-life.core
+  (:require [clojure.core.match :refer [match]])
   (:gen-class))
+
+; Cell rules
+
+(defn alive? [cell] (= cell :alive))
+(defn dead? [cell] (= cell :dead))
+
+(defn tick-cell [cell numberOfAliveNeighbours]
+  (match [cell numberOfAliveNeighbours]
+    [:alive 2] :alive
+    [_ 3] :alive
+    :else :dead))
 
 ; Main
 
--- grid/test/game_of_life/core_test.clj
+++ grid/test/game_of_life/core_test.clj
@@ -2,3 +2,30 @@
   (:require [clojure.test :refer :all]
             [game-of-life.core :refer :all]
             [game-of-life.grid-from-string :refer :all]))
+
+(deftest rules
+
+  (testing "Any live cell with fewer than two live neighbours dies, as if by underpopulation."
+    (is (->> [0, 1]
+             (mapv (partial tick-cell :alive))
+             (every? dead?))))
+
+  (testing "Any live cell with two or three live neighbours lives on to the next generation."
+    (is (->> [2, 3]
+             (mapv (partial tick-cell :alive))
+             (every? alive?))))
+
+  (testing "Any live cell with more than three live neighbours dies, as if by overpopulation."
+    (is (->> [4, 5, 6, 7, 8]
+             (mapv (partial tick-cell :alive))
+             (every? dead?))))
+
+  (testing "Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction."
+    (is (= :alive
+           (tick-cell :dead 3))))
+
+  ; The missing rule
+  (testing "Any dead cell without exactly three live neighbours stays dead."
+    (is (->> [0, 1, 2, 4, 5, 6, 7, 8]
+             (mapv (partial tick-cell :dead))
+             (every? dead?)))))
